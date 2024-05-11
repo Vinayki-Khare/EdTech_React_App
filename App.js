6583b3f36617c1b5e9c2a9cc21231ec8e0da2952
vinayki <kharevinayki7@gmail.com>
2024-05-11 22:31:07 +0530

    worked on make class screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Course from './src/students/Course';
+import MakeClass from './src/teacher/MakeClass';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <MakeClass/>
     </View>
   );
 }
